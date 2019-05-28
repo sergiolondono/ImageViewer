9e195aa9d9751086d88b427d5ae72fef967e09bf
Londono Gallo Sergio Andres <Sergio.Londono@carvajal.com>
2019-05-28 16:40:43 -0500

    20190528. Se obtienen bytes desde servicio y se muestra en la vista.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ImageViewerModule } from 'ng2-image-viewer';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     ImageViewerModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
